11a02567cef6e04eb992dca215b0be84a8ccb1aa
rallysport206 <phan.tony125@gmail.com>
2018-08-30 14:58:02 -0700

    added css

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -14,13 +14,14 @@ class Footer extends Component {
         <footer>
           <div className="row">
               <div className="twelve columns">
-                <ul className="social-links">
-                    {networks}
-                </ul>
+                <h3 className="responsive-footer">Lets Work Together!</h3>
+                  <ul className="social-links">
+                      {networks}
+                  </ul>
 
-                <ul className="copyright">
-                    <li>&copy; Copyright 2018 Tony Phan</li>
-                </ul>
+                  <ul className="copyright">
+                      <li>&copy; Copyright 2018 Tony Phan</li>
+                  </ul>
 
               </div>
               <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open"></i></a></div>
