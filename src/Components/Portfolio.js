6e8c8e03d3126325ace4338805b7b9963be2a14a
rallysport206 <phan.tony125@gmail.com>
2018-09-17 23:01:53 -0700

    updated link for Breakout project

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -32,7 +32,7 @@ class Portfolio extends Component {
 
             <h1>Check Out Some of My Works.</h1>
 
-            <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+            <div target="_blank" id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
                 {projects}
             </div>
           </div>
